fe7235ddca85c6ee8d2cccaf34e974ae26b300fa
ifeanyi-ajimah <ajimahifeanyi@gmail.com>
2020-10-01 06:13:20 +0100

    installed the gull theme on my vue cli. yipeeeee

---
+++ src/views/Sample.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sample">
+        <div class="container">
+          <h3> sample Page</h3>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sample',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ 
+</style>

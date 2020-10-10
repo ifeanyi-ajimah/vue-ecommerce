d68da50f73c5593de6e99bc4d2357fa5694c477d
ifeanyi-ajimah <ajimahifeanyi@gmail.com>
2020-10-10 23:51:22 +0100

    updated product

--- src/components/GullProducts.vue
+++ src/components/GullProducts.vue
@@ -14,38 +14,57 @@
       <hr>
       <h2> Create Read  Delete and Update </h2>
       <div class="row justify-content-center align-content-center">
+        <div class="col-md-12">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <div class="card-title mb-3">Products Inputs</div>
+                        <form>
+                            <div class="row">
+                                <div class="col-md-12 form-group mb-3">
+                                    <label for="firstName1"> Product name</label>
+                                    <input class="form-control" v-model="product.name" id="firstName1" type="text" placeholder="Enter product name " />
+                                </div>
+                                <div class="col-md-12 form-group mb-3">
+                                    <label for="lastName1"> Product Price </label>
+                                    <input class="form-control" v-model="product.price" id="lastName1" type="text" placeholder="Enter product price" />
+                                </div>
 
-          <div class="col-md-12">
-                        <div class="card mb-4">
-                            <div class="card-body">
-                                <div class="card-title mb-3">Products Inputs</div>
-                                <form>
-                                    <div class="row">
-                                        <div class="col-md-12 form-group mb-3">
-                                            <label for="firstName1"> Product name</label>
-                                            <input class="form-control" v-model="product.name" id="firstName1" type="text" placeholder="Enter product name " />
-                                        </div>
-                                        <div class="col-md-12 form-group mb-3">
-                                            <label for="lastName1"> Product Price </label>
-                                            <input class="form-control" v-model="product.price" id="lastName1" type="text" placeholder="Enter product price" />
-                                        </div>
+                                <!-- <div class="col-md-12 form-group mb-3">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input class="form-control" id="exampleInputEmail1" type="email" placeholder="Enter email" />
+                                    <!--  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --
+                                </div> -->
+                              
 
-                                        <!-- <div class="col-md-12 form-group mb-3">
-                                            <label for="exampleInputEmail1">Email address</label>
-                                            <input class="form-control" id="exampleInputEmail1" type="email" placeholder="Enter email" />
-                                            <!--  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --
-                                        </div> -->
-                                     
-
-                                        <div class="col-md-12">
-                                            <button @click.prevent="saveData" class="btn btn-primary">Submit</button>
-                                        </div>
-                                    </div>
-                                </form>
+                                <div class="col-md-12">
+                                    <button @click.prevent="saveData" class="btn btn-primary">Submit</button>
+                                </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
-
+                </div>
+        </div>
+      </div>
+      <hr>
+   <h2> Products </h2>
+      <div class="row justify-content-center">
+           
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th> Price </th>
+                  <th> index </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(prod, index) in products" :key="index">
+                  <td >  {{ prod.name}}  </td>
+                  <td> {{ prod.price}} </td>
+                  <td> {{ index}} </td>
+                </tr>
+              </tbody>
+            </table>
       </div>
     </div>
 
@@ -60,12 +79,15 @@ export default {
   props: {
     msg: String
   },
+ 
   data(){
     return{
+      products: [],
       product :{
         name : '',
         price : ''
-      }
+      },
+
     }
 
   },
@@ -74,8 +96,11 @@ export default {
       saveData(){
         db.collection("Products").add( this.product )
           .then((docRef) => {   // ensure to always use fat arrow function in your promise, so that you can be able to call other functions using the this keyword
-              this.reset();
-              console.log("Document written with ID: ", docRef.id);
+             
+            this.getData();
+              // console.log( docRef );
+              //  this.reset();
+              // console.log("Document written with ID: ", docRef.id);
           })
           .catch(function(error) {
               console.error("Error adding document: ", error);
@@ -83,8 +108,25 @@ export default {
       },
       reset(){
         Object.assign(this.$data, this.$options.data.apply(this));
+      },
+
+      getData(){
+
+          db.collection("Products").get().then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+              // doc.data() is never undefined for query doc snapshots
+              this.products.push( doc.data() );
+              console.log(doc.id, " => ", doc.data());
+          });
+          });
+          
       }
-  }
+
+  },
+   mounted(){
+    this.getData();
+  },
+
 
 }
 </script>
